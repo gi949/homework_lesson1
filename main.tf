f3819d53eefbe0037b6fb2d7d7b955767d05e04b
root <vaterlo355@yandex.ru>
2023-11-22 11:18:04 +0300

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "yandex_compute_instance" "vm-1" {
   }
 
   metadata = {
-    ssh-keys = "root:${file("~/.ssh/id_rsa.pub")}"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 }
 
